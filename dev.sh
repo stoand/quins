7a2b730c954b7e1eaf16f6c456755ff61dc89353
Andreas Stocker <shadowstep7@gmail.com>
2022-08-13 14:45:58 +0200

    examples-forum: watch library for changes when running example dev script

--- dev.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nodemon -e idr,ipkg -x 'rm build -rf; idris2 --build hello-idris-js.ipkg'
--- examples/forum/dev.sh
+++ examples/forum/dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-idris2 --install ../../quins.ipkg
-nodemon -e idr,ipkg -x 'rm build -rf; idris2 --build quins-forum.ipkg'
+
+cd ../../ && nodemon -e idr,ipkg -x \
+    'rm build -rf; idris2 --install quins.ipkg && cd examples/forum/ && rm build -rf; idris2 --build quins-forum.ipkg'
