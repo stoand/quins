bde7be663261b7ff56257e015cadefbda7d022c1
Andreas Stocker <shadowstep7@gmail.com>
2022-08-13 16:36:26 +0200

    examples-forum: dev script starts server

--- examples/forum/dev.sh
+++ examples/forum/dev.sh
@@ -2,4 +2,4 @@
 
 cd ../../ && nodemon -e idr,ipkg -x \
     'rm build -rf; idris2 --install quins.ipkg && \
-     cd examples/forum/ && rm build -rf; idris2 --build quins-forum.ipkg'
+     cd examples/forum/ && rm build -rf; idris2 --build quins-forum.ipkg && node ./build/exec/quins-forum.js'
