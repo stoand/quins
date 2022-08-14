4d1e9542770e1e26962e134b9150a156c5091d74
Andreas Stocker <shadowstep7@gmail.com>
2022-08-14 10:34:10 +0200

    dev-build: do not remove build dir

--- examples/forum/dev.sh
+++ examples/forum/dev.sh
@@ -2,7 +2,7 @@
 
 cd ../../ && nodemon -e idr,ipkg -x \
     'rm build -rf; idris2 --install quins-backend.ipkg; idris2 --install quins-frontend.ipkg && \
-     cd examples/forum/ && rm build -rf; \
+     cd examples/forum/ && \
      idris2 --build quins-forum-backend.ipkg && \
      idris2 --build quins-forum-frontend.ipkg && \
      node ./build/exec/quins-forum-backend.js'
